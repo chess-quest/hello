76e0f541a390919a9872453905b1c1e5b3946b4e
Tony <tthompson55@huskers.unl.edu>
2022-01-27 16:27:35 +0000

    actually put code in hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Tony Thompson
+ * Date: 1/27/22
+ * 
+ * A simple program that prints my name and major
+ */
+ 
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Tony Thompson\nElectrical Engineering\n");
+     
+     return 0;
+     
+ }
